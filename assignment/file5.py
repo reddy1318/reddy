2c3f0c9896de9a35be8ac70af8096c4f03de64c1
reddy1318 <rushireddy.marolix@gmail.com>
2023-09-14 21:57:16 +0530

    initial commit

--- assignment/file5.py
+++ assignment/file5.py
@@ -12,13 +12,13 @@ sample_string='The quick Brow Fox'
 result=count_case_characters(sample_string)
 print(result)
 
-#2
+#2.Write a Python function that takes a list and returns a new list with distinct elements from the first list.
 def get_unique_elements(lst):
     return list(set(lst))
 sample_list=[1,2,3,3,3,3,4,5]
 unique_list=get_unique_elements(sample_list)
 print(unique_list)    
-#3
+#3Write a Python function to check whether a string is a pangram or not.
 def is_pangram(string):
     string=string.lower()
     alphabet=set()
@@ -29,13 +29,13 @@ def is_pangram(string):
 print(is_pangram("The quick brown fox jumps over the lazy dog"))
 
 
-#4
+#4.Write a Python function to create and print a list where the values are the squares of numbers between 1 and 10 (both included).
 def create_square_list():
     square_list=[x**1 for x in range(1,11)]
     return square_list
 squares=create_square_list()
 print(squares)
-#5
+#5.Write a Python function to sum all the numbers in a list.
 def sum_of_list(numbers):
     total=0
     for num in numbers:
@@ -44,7 +44,8 @@ def sum_of_list(numbers):
 sample_list=[8,2,3,0,7]
 result=sum_of_list(sample_list)
 print(result)
-#6
+#6write a function to find sum of given values, pass values has variable number of arguments to function.
+
 def sum_of_values(*args):
     total=0
     for value in args:
---
+++ assignment/file8Employee.py
@@ -0,0 +1,19 @@
+employee_name=[]
+domain=[]
+employee_id=[]
+email=[]
+n=int(input("Enter the no.of rows:"))
+for x in range(n):
+    a=input("Enter the employee name:")
+    employee_name.append(a)
+    b=input("Enter the domain:")
+    domain.append(b)
+    c=input("Enter the employee_id:")
+    employee_id.append(c)
+    d=input("Enter the email:")
+    email.append(d)
+print()
+print('employee_name','domain','employee_id','email')
+for a1,a2,a3,a4 in zip(employee_name,domain,employee_id,email):
+    print(a1,a2,a3,a4,sep="\t\t")
+print()
