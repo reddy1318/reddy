01bc10b776e6edfa2912046e8b7c52eb7fbc66c3
reddy1318 <141002398+reddy1318@users.noreply.github.com>
2023-09-20 09:15:05 +0530

    Update employeefile9.py

--- assignment/file9.py
+++ assignment/file9.py
@@ -1,49 +1,15 @@
-# def add_employees():
-#     java=[]
-#     python=[]
-#     sap=[]
-#     n=int(input("Enter the num_of_employees"))
-#     for i in range(n):
-#         name=input("Enter the name:")
-#         empid=input("Enter the empid:")
-#         domain=input("Enter the domain:")
-#         email=input("Enter the email:")
-#         employee={"name":name,"emplid":empid,"email":email}
-#         if domain=="java":   
-#             java.append=(employee)
-#         elif domain=="python":
-#             python.append(employee)
-#         elif domain=="sap":
-#             sap.append(employee)
-#         else:
-#             print("invalid domain")
-#     print("java employee:")
-#     for emp in java:
-#         print(emp)
-#     print("python employee:")
-#     for emp in python:
-#         print(emp)
-#     print("sap:") 
-#     for emp in sap:
-#         print(emp)
-# add_employees()
-
 
 def add_employees():
     java = []
     python = []
-    sap = []
-    
-    n = int(input("Enter the number of employees: "))
-    
+    sap = [] 
+    n = int(input("Enter the number of employees: ")) 
     for i in range(n):
         name = input("Enter the name: ")
         empid = input("Enter the empid: ")
         domain = input("Enter the domain: ")
         email = input("Enter the email: ")
-
         employee = {"name": name, "empid": empid, "email": email}
-        
         if domain == "java":
             java.append(employee)
         elif domain == "python":
@@ -53,15 +19,13 @@ def add_employees():
         else:
             print("Invalid domain. Skipping employee.")
 
-    print("\nJava Employees:")
+    print("Java Employees:")
     for emp in java:
-        print(emp)
-    
-    print("\nPython Employees:")
+        print(emp)  
+    print("Python Employees:")
     for emp in python:
         print(emp)
-    
-    print("\nSAP Employees:")
+    print("SAP Employees:")
     for emp in sap:
         print(emp)
 
